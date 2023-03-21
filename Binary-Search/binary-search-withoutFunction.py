4f75fcfc1f1ea4657ecae742b128b08af8ad1629
WasathTheekshana <wasath.vt@gmail.com>
2023-03-21 12:08:03 +0530

    update: Binary Search

---
+++ Binary-Search/binary-search-withoutFunction.py
@@ -0,0 +1,23 @@
+list = [3, 5, 10, 11, 20, 22, 51, 76, 101] #Sorted List
+searchKey = 20
+flag = 0
+
+lowerBoundary = 0
+upperBoundary = len(list) - 1
+
+
+while upperBoundary > lowerBoundary & flag == 0:
+    midInedx = ( lowerBoundary + upperBoundary ) // 2
+    if list[midInedx] == searchKey:
+        print("Item found in index", midInedx)
+        globals() ['flag'] == 1
+        break
+
+    elif searchKey > list[midInedx]:
+        lowerBoundary = midInedx + 1
+
+    elif searchKey < list[midInedx]:
+        upperBoundary = midInedx - 1
+
+if flag == 1:
+    print("Item not found")
